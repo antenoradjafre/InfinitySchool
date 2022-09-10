63de7207931ed9e1f45662ce98c7c3f87b89b18f
Antenor <aadjafre@gmail.com>
2022-09-09 22:14:08 -0300

    Adicionando exibir Usuario e nova tela

--- Python/main.py
+++ Python/main.py
@@ -13,6 +13,7 @@ banco = mysql.connector.connect(
 def cadastrarUsuario():
     cadastroUsuarioView.show()
     cadastroUsuarioView.pushButton.clicked.connect(salvarUsuario)
+    cadastroUsuarioView.pushButton_2.clicked.connect(exibirUsuario)
 
 
 def salvarUsuario():
@@ -23,6 +24,21 @@ def salvarUsuario():
     dados = (str(linhaUm), str(linhaDois))
     cursor.execute(comandoSql, dados)
     banco.commit()
+
+
+def exibirUsuario():
+    exibirUsuarioView.show()
+    cursor = banco.cursor()
+    comandoSql = f'select * from usuario'
+    cursor.execute(comandoSql)
+    dados_lidos = cursor.fetchall()
+
+    exibirUsuarioView.tableWidget.setRowCount(len(dados_lidos))
+    exibirUsuarioView.tableWidget.setColumnCount(3)
+
+    for i in range(0, len(dados_lidos)):
+        for j in range(0,3):
+            exibirUsuarioView.tableWidget.setItem(i,j, QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
 
 def cadastrarProduto():
@@ -54,6 +70,7 @@ app = QtWidgets.QApplication([])
 telaInicial = uic.loadUi('View/TelaInicial.ui')
 cadastroUsuarioView = uic.loadUi('View/CadastroUsuario.ui')
 cadastroProdutoView = uic.loadUi('View/CadastroProduto.ui')
+exibirUsuarioView = uic.loadUi('View/ExibirUsuario.ui')
 telaInicial.pushButton.clicked.connect(cadastrarUsuario)
 telaInicial.pushButton_2.clicked.connect(cadastrarProduto)
 
